23ecb682e73b333fe2814f0f8e6bd5d780d63082
SerhiiPavlichuk <s_pavlichuk@icloud.com>
2025-09-24 19:35:51 +0300

    project init

---
+++ ObrioTestTask/ObrioTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObrioTestTask
+//
+//  Created by Serhii on 24.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
